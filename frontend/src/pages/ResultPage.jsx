e9c461d3d10e077111074f6ab86833703b554595
othiel <othiel.leith@gmail.com>
2023-11-15 09:37:28 +0100

    filterAll

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -7,8 +7,7 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Cardcarresult from "../components/Cardcarresult";
-// import filterCars from "../services/carFilterServices";
-// import CarmodelResult from "./CarmodelResult";
+import filterCars from "../services/carFilterServices";
 
 export default function ResultPage() {
   const [cars, setCars] = useState([]);
@@ -41,47 +40,7 @@ export default function ResultPage() {
     setKilometer(event.target.value);
   };
 
-  // const filteredCars = filterCars(cars, motorisation, price, kilometer);
-
-  const filteredCars = cars.filter((car) => {
-    const motorisationFilter =
-      (motorisation === "Diesel" && car.motorisation === "Diesel") ||
-      (motorisation === "Petrol" && car.motorisation === "Petrol") ||
-      motorisation === "";
-
-    const priceFilter =
-      (price === "9999" && car.price <= 10000) ||
-      (price === "19999" && car.price > 10000 && car.price <= 20000) ||
-      (price === "29999" && car.price > 20000 && car.price <= 30000) ||
-      (price === "39999" && car.price > 30000 && car.price <= 40000) ||
-      (price === "49999" && car.price > 40000 && car.price <= 50000) ||
-      (price === "50000" && car.price > 50000) ||
-      price === "";
-
-    const kilometerFilter =
-      (kilometer === "9999" &&
-        car.kilometer >= 1000 &&
-        car.kilometer <= 10000) ||
-      (kilometer === "19999" &&
-        car.kilometer > 10000 &&
-        car.kilometer <= 20000) ||
-      (kilometer === "29999" &&
-        car.kilometer > 20000 &&
-        car.kilometer <= 30000) ||
-      (kilometer === "39999" &&
-        car.kilometer > 30000 &&
-        car.kilometer <= 40000) ||
-      (kilometer === "49999" &&
-        car.kilometer >= 40000 &&
-        car.kilometer < 50000) ||
-      (kilometer === "59999" &&
-        car.kilometer >= 49999 &&
-        car.kilometer <= 59999) ||
-      (kilometer === "69999" && car.kilometer > 60000) ||
-      kilometer === "";
-
-    return motorisationFilter && priceFilter && kilometerFilter;
-  });
+  const filteredCars = filterCars(cars, motorisation, price, kilometer);
 
   return (
     <>
